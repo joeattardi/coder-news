cd00536d948b2b0fbf9dece4fb78e06c076b10ed
Joe Attardi <joe@attardi.net>
2016-11-17 22:54:07 -0500

    Add required validator, internal method to Story

--- server/models/Story.js
+++ server/models/Story.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const storySchema = new mongoose.Schema({
-  title: String,
-  url: String,
+  title: {
+    type: String,
+    required: [true, 'Title is required']
+  },
+  url: {
+    type: String,
+    required: [true, 'URL is required']
+  },
   domain: String,
   votes: Number,
   submitted: Date
 });
+
+storySchema.methods.relativeSubmitted = function relativeSubmitted() {
+  return moment(this.submitted).fromNow(); 
+};
 
 module.exports = mongoose.model('Story', storySchema);
